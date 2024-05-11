aaa8d67ce3d404dacc4f8f25f9485d1b9ab19413
opinon1 <oscar.pinon3@gmail.com>
2024-05-11 14:15:05 -0600

    initial

---
+++ factorial.lua
@@ -0,0 +1,13 @@
+local function factorial (n)
+  if n == 0 then
+    return 1
+  else 
+    return n * factorial(n-1)
+  end
+end
+
+print("enter a number")
+local n = io.read("*n") -- number
+print(factorial(n))
+
+
